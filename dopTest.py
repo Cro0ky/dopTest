6e5ef0599397de2142933e4a000460f0027eea73
Cro0ky <143035135+Cro0ky@users.noreply.github.com>
2023-10-10 22:58:34 +0300

    Add files via upload

---
+++ dopTest.py
@@ -0,0 +1,35 @@
+#task 1
+# num = int(input('Введите число: '))
+#
+# if num % num == 0 and num % 1 == num and :
+#     print('Число простое!')
+# else:
+#     print("Число сложное")
+#task 2
+# list = [1, 4, 3, 6, 7, 0, 10, 8, 3, 2]
+# n = len(list)
+
+# for i in range(0, n-1):
+#     for j in range(0, n-1-i):
+#         if list[j] > list[j+1]:
+#             list[j], list[j+1] = list[j + 1], list[j]
+# print(list)
+#task 3
+# a = [1, 4, 3, 6, 7, 0, 8, 3, 2]
+# b = 0
+# num = 0
+
+# for i in a:
+#     if i > num:
+#         b = i
+#         num = i
+# print(b)
+#task4
+# n = int(input())
+# x0 = 0
+# x1 = 1
+# for i in range(n-1):
+#     x2 = x1 + x0
+#     x1, x0 = x2, x1
+#     print(x2)
+#task5
